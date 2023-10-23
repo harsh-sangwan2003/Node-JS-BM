8de5bb69caffeeaf421caf94786b01cef40f06fe
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-10-23 08:07:29 +0530

    changes

--- task-manager-api/utils/token.js
+++ task-manager-api/utils/token.js
@@ -5,14 +5,15 @@ module.exports = {
     SECRET: process.env.SECRET,
     generateToken(emailid) {
 
-        let tokenId = jwt.sign({ userid: emailid }, this.SECRET, { expiresIn: '1h' });
-        return tokenId;
+        let tokenId = jwt.sign({ userid: emailid }, process.env.SECRET, { expiresIn: '24h' });
+        console.log(tokenId);
+        return tokenId
     },
     verifyToken(tokenId) {
 
         try {
 
-            let decode = jwt.verify(tokenId, this.SECRET);
+            let decode = jwt.verify(tokenId, process.env.SECRET);
             return decode && decode.emailid;
 
         } catch (err) {
